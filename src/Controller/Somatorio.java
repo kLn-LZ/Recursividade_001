359c5b0e712fff50a530cfbf79d4a6c1e41876b4
kLn-LZ <160866564+kLn-LZ@users.noreply.github.com>
2024-02-25 20:07:42 -0300

    Adicionando comentários

--- src/Controller/Somatorio.java
+++ src/Controller/Somatorio.java
@@ -8,17 +8,21 @@ public class Somatorio {
     }
 
 
-  public int somFunc (int n) {
+    public int somFunc (int n) {
 
+        //Condição de parada: ser um número negativo (ou seja, menor do que zero)
         if (n < 0) {
 
             return 0;
 
         }
 
+        // Chamada dos passos: Váriavel "n" somando com o retorno da função com o parâmetro - 1 (cujo "n", vai ser o
+        // return da última função chamada na iteração
         return n + somFunc(n - 1);
 
     }
+
 
 
 }
